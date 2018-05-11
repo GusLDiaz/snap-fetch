dcd322aefb24e95188724af20130879f2a469c85
Gus Liakos <Gliakos@cnm.edu>
2018-05-11 09:30:31 -0600

    reset function order

--- sfetch.js
+++ sfetch.js
@@ -2,21 +2,22 @@
 // fetch('https://jsonplaceholder.typicode.com/posts/1')
 // 	.then(response => response.json())
 // 	.then(json => console.log(json))
-function putOnWeb(x) {
+function putOnWeb(x){
 	document.getElementById('here').innerText = x;
 }
+function btnFetch() {
+	fetch('https://jsonplaceholder.typicode.com/posts', {
+		method: 'POST',
+		body: JSON.stringify({
+			title: 'Gus',
+			body: 'thereismagicinthetheatreandthetheatreismagic',
+			userId: 1
+		}),
+		headers: {
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+		.then(response => response.json());
+		response.json().then(putOnWeb(response.json))
+			}
 
-fetch('https://jsonplaceholder.typicode.com/posts', {
-	method: 'POST',
-	body: JSON.stringify({
-		title: 'Gus',
-		body: 'thereismagicinthetheatreandthetheatreismagic',
-		userId: 1
-	}),
-	headers: {
-		"Content-type": "application/json; charset=UTF-8"
-	}
-})
-
-	.then(response => response.json())
-	.then(json => putOnWeb(json));
