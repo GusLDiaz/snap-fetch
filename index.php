833a4ca3ba62ec0cadd68118a7e15bea8aa81631
Gus Liakos <Gliakos@cnm.edu>
2018-05-11 09:08:50 -0600

    add corresponding html

--- index.php
+++ index.php
@@ -5,4 +5,19 @@
  * Date: 5/11/2018
  * Time: 8:34 AM
  */
-//if $method = "POST"
+ if $method = "POST"
+ ?>
+ <!DOCTYPE html>
+ <html>
+ <head>
+ <meta charset="utf-8">
+ <script src="sfetch.js"></script>
+</head>
+<body>
+<div class = "container">
+<p class = "here">
+
+</p>
+</div>
+</body>
+</html>
