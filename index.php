2fcac3366307e43e7aaf702a6a787d759ac094be
Gus Liakos <Gliakos@cnm.edu>
2018-05-15 15:44:58 -0600

    syntax fix re brackets

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'vendor/autoload.php';
-
+require_once __DIR__ . '/composer/autoload_real.php';
 use GuzzleHttp\Client;
 $client = new GuzzleHttp\Client(['base_uri' => 'https://www.hikingproject.com/data/']);
 
